5e1e0d19b1c99cde1c7b6bb5b6353031a59bcd4b
Madeleine Malmsten <madeleine@malmsten.eu>
2020-12-29 22:16:29 +0100

    Day 7: Amplification Circuit - not ready yet

--- puzzle7-step1.exs
+++ puzzle7-step1.exs
@@ -1,99 +1,57 @@
-# --------------------------------------------------------------------------------------------------
-# This is an Elixir implementation of Puzzle 5 (step 2). I'll continue working on this later. :)
-# --------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# Not working yet...
+# ----------------------------------------------------------------------
+
 defmodule Puzzle do
 
+# ----------------------------------------------------------------------
+# Start loop/2 in new process. Start run/2
+# ----------------------------------------------------------------------
 	def start do
-		{:ok, data} = File.read("/Users/madde/Sites/advent-of-code-2019/input/puzzle5.txt")
-		program = init_data(%{}, 0, String.split(data, ","))
-		run(program, 0)
+		Application.ensure_all_started(:inets)
+		Application.ensure_all_started(:ssl)
+		pid = spawn fn -> loop(0) end
+		run(pid, 0)
 	end
 
-	def init_data(map, _, []) do map end
-	def init_data(map, x, [h|t]) do
-		init_data(Map.put(map, x, String.to_integer(h)), x + 1, t)
-	end
-
-	def run(_, :done) do :ok end
-	def run(program, n) do
-		instruction = plot_optcode(Integer.to_string(Map.get(program, n)))
-		case instruction do
-
-			# Exit
-			<<_::binary-size(3),"99">> -> run(program, :done)
-
-            # Adds together numbers read from two positions and stores the result in a third
-            # position
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"1">> -> 
-				program1 = Map.put(program, Map.get(program, n + 3), 
-					mode(program, p1, n + 1) + mode(program, p2, n + 2))
-				run(program1, n + 4)
-
-            # Works exactly like opcode 1, except it multiplies the two inputs instead of adding
-            # them
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"2">> -> 
-				program1 = Map.put(program, Map.get(program, n + 3), 
-					mode(program, p1, n + 1) * mode(program, p2, n + 2))
-				run(program1, n + 4)
-
-			# Takes a single integer as input and saves it to the position given by its only
-			# parameter
-			<<_::binary-size(4),"3">> -> 
-				input = 5
-				program1 = Map.put(program, Map.get(program, n + 3), input)
-				run(program1, n + 2)
-
-			# Outputs the value of its only parameter
-			<<_::binary-size(2),p1::binary-size(1),_::binary-size(1),"4">> -> 
-				IO.puts(mode(program, p1, n + 1))
-				run(program, n + 2)
-
-			# If the first parameter is non-zero, it sets the instruction pointer to the value from
-			# the second parameter. Otherwise, it does nothing.
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"5">> -> 
-				case mode(program, p1, n + 1) do
-					0 -> run(program, n + 3)
-					_ -> run(program, mode(program, p2, n + 2))
+# ----------------------------------------------------------------------
+# Receive all final output signals from amplifier E's. Update result if
+# a higher output signal is received. Return result when all output
+# signals are received
+# ----------------------------------------------------------------------
+	def loop(result, 50000) do result end
+	def loop(result, n) do
+		receive do
+			{:ok, response} -> 
+				IO.puts(response)
+				case response > result do
+					true -> loop(response, n + 1)
+					_ -> loop(result, n + 1)
 				end
-
-			# If the first parameter is zero, it sets the instruction pointer to the value from the
-			# second parameter. Otherwise, it does nothing.
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"6">> -> 
-				case mode(program, p1, n + 1) do
-					0 -> run(program, mode(program, p2, n + 2))
-					_ -> run(program, n + 3)
-				end
-
-			# If the first parameter is less than the second parameter, it stores 1 in the position
-			# given by the third parameter. Otherwise, it stores 0.
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"7">> -> 
-				program1 = case mode(program, p1, n + 1) < mode(program, p2, n + 2) do
-					true -> Map.put(program, Map.get(program, n + 3), 1)
-					false -> Map.put(program, Map.get(program, n + 3), 0)
-				end
-				run(program1, n + 4)
-
-
-			# If the first parameter is equal to the second parameter, it stores 1 in the position
-			# given by the third parameter. Otherwise, it stores 0.
-			<<_::binary-size(1),p2::binary-size(1),p1::binary-size(1),_::binary-size(1),"8">> -> 
-				program1 = case mode(program, p1, n + 1) == mode(program, p2, n + 2) do
-					true -> Map.put(program, Map.get(program, n + 3), 1)
-					false -> Map.put(program, Map.get(program, n + 3), 0)
-				end
-				run(program1, n + 4)
-
 		end
 	end
 
-	def plot_optcode(code) do 
-		case String.length(code) do
-			l when l == 5 -> code
-			l when l < 5 -> plot_optcode(<<"0",code::binary>>)
-		end
+# ----------------------------------------------------------------------
+# Loop possible phase setting sequences
+# ----------------------------------------------------------------------
+	def run(_, 50000) do :ok end
+	def run(pid, n) do 
+		list = String.splitter(String.pad_leading(
+			to_string(n), 5, "0"), "", trim: true) |> Enum.take(5)
+		run(pid, list, 0)
+		run(pid, n + 1)
 	end
 
-	def mode(program, "1", pos) do Map.get(program, pos) end
-	def mode(program, "0", pos) do mode(program, "1", Map.get(program, pos)) end
+# ----------------------------------------------------------------------
+# Run phase setting sequence. Send the final output signal from 
+# amplifier E to loop/2
+# ----------------------------------------------------------------------
+	def run(pid, [], response) do send(pid, {:ok, response}) end
+	def run(pid, [h|t], input) do 
+		{:ok, {{'HTTP/1.1', 200, 'OK'}, _, response}} =
+			:httpc.request(:get, 
+				{'http://localhost:3000/7/#{h}-#{input}', []}, [], [])
+		run(pid, t, response)
+	end
 
 end
